72e24618f838465e9b96962044c7d2e0cd96b7c4
Khaled <khaled.m.hassan.94@gmail.com>
2020-10-13 14:32:50 +0200

    new files

---
+++ server.js
@@ -0,0 +1,48 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 3000;
+// Setup Server
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
+
+// Create JS array
+const tempData = [];
+// Respond with JS object when a GET request is made to the homepage
+app.get('/all', (req, res) => {
+    res.send(tempData);
+    console.log(tempData);
+});
+
+
+app.post('/addTemp', (req, res) => {
+    newEntry = {
+        city: req.body.city,
+        date: req.body.date,
+        temp: req.body.temp,
+        feeling: req.body.feeling,
+    }
+    tempData.push(newEntry);
+    res.send(tempData);
+    // console.log(tempData);
+});
---
+++ website/app.js
@@ -0,0 +1,80 @@
+/* Global Variables */
+const generate = document.getElementById('generate');
+const city = document.getElementById('city');
+const date = document.getElementById('date');
+const temp = document.getElementById('temp');
+const content = document.getElementById('content');
+
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '6fd0d8d8f6a15f72f4763189521cde7f';
+
+
+generate.addEventListener('click', (e) => {
+    const feeling = document.getElementById('feelings').value;
+    const zipCode =  document.getElementById('zip').value;
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let newDate = `${d.getMonth()+1}` + '.' + d.getDate() + '.' + d.getFullYear();
+    
+    getTemp(baseURL, zipCode, apiKey)
+    .then(
+        (data) =>
+            postData('/addTemp', {city:data.name, date:newDate, temp:data.main.temp, feeling})
+    )
+    .then(
+        updateUI
+    )
+    .catch((error) => {
+        console.error(error);
+        alert("Please enter a valid zipcode!");
+    });
+});
+
+const getTemp = async (baseURL, zipCode, apiKey)=>{
+    const res = await fetch(`${baseURL}${zipCode}&appid=${apiKey}&units=metric`);
+    // console.log(res);
+    try {
+        if (res.status === 404){
+            alert("wrong zipCode");
+            throw Error;
+        }
+        const data = await res.json(); // from JSON to variable
+        console.log(data);
+        return(data);
+    } catch (error) {
+        console.log("Wrong zip code!");
+    }
+}
+
+/* POST example */
+const postData = async ( url = '', data = {})=> {
+    const res = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), //turns JS objs and JSON data into a string for our server to receive the information
+    });
+    const newData = await res.json();
+    console.log(newData);
+    return newData;
+    
+}
+
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+      const allData = await request.json();
+      city.innerHTML = allData[allData.length-1].city;
+      date.innerHTML = allData[allData.length-1].date;
+      temp.innerHTML = allData[allData.length-1].temp;
+      content.innerHTML = allData[allData.length-1].feeling;
+  
+    }catch(error){
+    //   console.log("error", error);
+      if (res.status === 404){
+          alert("wrong zipCode");
+      }
+    }
+}
